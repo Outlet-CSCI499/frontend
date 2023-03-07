34d65d2a105b2cdeed60726b262eb6550e40a7f9
Tyler Robinson <tylerrhb11@gmail.com>
2023-03-06 23:37:37 -0500

    news articles route to specific article id's

--- src/App.js
+++ src/App.js
@@ -13,13 +13,12 @@ import {
 
 
 function App() {
-  //@TODO for news article path, make it so that the path changes based on article ID
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePageContainer} />
         <Route exact path="/feed" component={NewsFeedContainer} />
-        <Route exact path ="article_no" component={NewsArticleContainer} />
+        <Route exact path ="article/:id" component={NewsArticleContainer} />
         <Route exact path="/signup" component={SignUpContainer} />
         <Route exact path="/login" component={LoginContainer} />
       </Switch>
