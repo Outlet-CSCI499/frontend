738838f946682e3926e13a8a3ea312103592debd
henrycheung1 <henrycheung1234567890@gmail.com>
2023-03-26 18:54:50 -0400

    Placeholder Home page
    placeholder home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,31 @@
 import "./index.scss";
-import LogoTitle from "../../assets/images/logo-o.png";
+import LogoTitle from "../../assets/images/logo-outlet1.png";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div className="container home-page"></div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="heading">
+        <h1>Welcome to</h1>
+        <img src={LogoTitle} height={500} width={800}></img>
+      </div>
+      <div className="registration">
+        <input
+          type="button"
+          className="loginButton"
+          value={"Login"}
+          onClick={() => navigate("/Login")}
+        ></input>
+        <input
+          type="button"
+          className="signButton"
+          value={"Sign Up"}
+          onClick={() => navigate("/SignUp")}
+        ></input>
+      </div>
+    </>
+  );
 };
 
 export default Home;
